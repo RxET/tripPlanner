0cd03178c9cf45f01bed53497070552f6c57ebba
Roxie Turner <roxie.turner@gmail.com>
2018-01-30 13:02:47 -0500

    Basics set up

--- src/index.js
+++ src/index.js
@@ -12,13 +12,5 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
-// const markerDomEl = document.createElement('div');
-// markerDomEl.style.width = "32px";
-// markerDomEl.style.height = "39px";
-// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
-
-// new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
-
-// console.log(buildMarker(null, [-74.009151, 40.705086]));
 const marker = buildMarker("Activity", [-74.009151, 40.705086]); // or [-87.6354, 41.8885]
 marker.addTo(map);
